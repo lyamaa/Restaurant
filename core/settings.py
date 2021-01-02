668857eb414078932a6d56c38d2fa6a877e4030d
surajmt8848 <suraj8848mt@gmail.com>
2021-01-02 17:17:08 +0545

    config static files

--- core/settings.py
+++ core/settings.py
@@ -119,4 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'media',
+]
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = BASE_DIR / 'static_cdn'
+MEDIA_ROOT = BASE_DIR / 'media_cdn'
+
