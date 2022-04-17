80ef238871989fad648142ff256bff08ca2a84d0
Sourcery AI <>
2022-04-17 05:46:35 +0000

    'Refactored by Sourcery'

--- food/social_auth_pipeline.py
+++ food/social_auth_pipeline.py
@@ -1,10 +1,10 @@
 from .models import Customer, Driver
 
 def create_user_by_type(backend, user, request, response, *args, **kwargs):
-    avatar = 'https://graph.facebook.com/%s/picture?type=large' % response['id']
+    avatar = f"https://graph.facebook.com/{response['id']}/picture?type=large"
     if backend.name == 'facebook':
         avatar = avatar
-    
+
     if request['user_type'] == 'driver' and not Driver.objects.filter(user_id=user.id):
         Driver.objects.create(user_id = user.id, avatar=avatar)
     elif not Customer.objects.filter(user_id = user.id):
